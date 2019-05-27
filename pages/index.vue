df9f78581ab0f4920b49c3b133a3ca77c9e11eb8
dailcoyote <dailcoyote@gmail.com>
2019-05-27 21:10:27 +0600

    +UserDataTable: hide/show columns

--- pages/index.vue
+++ pages/index.vue
@@ -23,28 +23,52 @@
               <v-btn icon>
                 <v-icon>filter_list</v-icon>
               </v-btn>
+              <!-- <v-btn icon>
+                <v-icon>more_vert</v-icon>
+              </v-btn>-->
+              <v-menu
+                offset-y
+                origin="center center"
+                :nudge-right="140"
+                :nudge-bottom="10"
+                transition="scale-transition"
+              >
+                <v-btn icon slot="activator">
+                  <v-icon>more_vert</v-icon>
+                </v-btn>
+                <v-list id="context_menu">
+                  <template v-for="(column, index) in complex.columnsVisible">
+                    <v-list-tile :key="index">
+                      <v-list-tile-content>
+                        <v-checkbox
+                          primary
+                          v-model="complex.columnsSelected"
+                          :label="column"
+                          :value="column"
+                        ></v-checkbox>
+                      </v-list-tile-content>
+                    </v-list-tile>
+                  </template>
+                </v-list>
+              </v-menu>
             </v-toolbar>
             <v-divider></v-divider>
             <v-card-text class="pa-0">
               <v-data-table
-                :headers="complex.headers"
+                :headers="computedHeaders"
                 :items="complex.items"
                 class="elevation-1"
-                v-model="complex.selected"
                 hide-actions
                 must-sort
               >
                 <template slot="items" slot-scope="props">
-                  <!-- <td>
-                    <v-checkbox primary hide-details v-model="props.selected"></v-checkbox>
-                  </td>-->
                   <td>{{ props.index + 1 }}</td>
-                  <td>{{ props.item.firstname }}</td>
-                  <td>{{ props.item.lastname }}</td>
-                  <td>{{ props.item.age }}</td>
-                  <td>{{ props.item.address.full }}</td>
-                  <td>{{ props.item.email }}</td>
-                  <td>{{ props.item.phone }}</td>
+                  <td v-if="complex.columnsSelected.indexOf('Firstname') != -1">{{ props.item.firstname }}</td>
+                  <td v-if="complex.columnsSelected.indexOf('Lastname') != -1">{{ props.item.lastname }}</td>
+                  <td v-if="complex.columnsSelected.indexOf('Age') != -1">{{ props.item.age }}</td>
+                  <td v-if="complex.columnsSelected.indexOf('Address') != -1">{{ props.item.address.full }}</td>
+                  <td v-if="complex.columnsSelected.indexOf('Email') != -1">{{ props.item.email }}</td>
+                  <td v-if="complex.columnsSelected.indexOf('Phone') != -1">{{ props.item.phone }}</td>
                   <td>
                     <v-btn depressed outline icon fab dark color="primary" small>
                       <v-icon>edit</v-icon>
@@ -90,7 +114,15 @@ export default {
       loading: false,
       mounted: false,
       complex: {
-        selected: [],
+        columnsVisible: [
+          "Firstname",
+          "Lastname",
+          "Age",
+          "Address",
+          "Email",
+          "Phone"
+        ],
+        columnsSelected: [],
         headers: [
           {
             text: "№",
@@ -138,9 +170,10 @@ export default {
   },
   mounted() {
     this.mounted = true;
-    window.addEventListener("scroll", () => {
-      this.bottom = this.bottomVisible();
-    });
+    (this.complex.columnsSelected = [...this.complex.columnsVisible]),
+      window.addEventListener("scroll", () => {
+        this.bottom = this.bottomVisible();
+      });
   },
   methods: {
     async fetchUsers() {
@@ -186,6 +219,12 @@ export default {
     }
   },
   computed: {
+    computedHeaders() {
+      return this.complex.headers.filter(head => {
+        if (head.text === "№" || head.text === "Action") return true;
+        return this.complex.columnsSelected.indexOf(head.text) != -1;
+      });
+    },
     fabBtnStyle() {
       return "top: 15px; right: 25px;";
     }
